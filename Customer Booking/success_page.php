03c3ac68110b39369fffde9c7205d25f30095eae
adam-1105 <57124150+adam-1105@users.noreply.github.com>
2023-07-03 10:37:47 +0800

    Add files via upload

---
+++ Customer Booking/success_page.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Parking Booking - Success</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        /* Styles */
+        body {
+            background-image: url("parking 1.jpg");
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
+
+        .container {
+            margin-top: 50px;
+        }
+
+        .card {
+            margin-bottom: 20px;
+        }
+		
+		.success-card-header {
+            background-color: #0d6efd;
+            border-radius: 10px 10px 0px 0px;
+            color: #fff;
+            padding: 20px;
+            text-align: center;
+        }
+    </style>
+</head>
+
+<body>
+    <div class="container">
+        <div class="card">
+            <div class="card-header success-card-header">
+                <h4>Parking Booked Successfully</h4>
+            </div>
+            <div class="card-body">
+                <p>Your parking has been successfully booked. Thank you!</p>
+                <a href="login.php" class="btn btn-primary">Back to Login</a>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
